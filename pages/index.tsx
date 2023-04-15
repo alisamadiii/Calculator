8f5bcb70b69e3c52ad670cbb5e102be1a1334bf8
Ali Reza <webdeve1083@gmail.com>
2023-04-15 12:36:06 +0800

    Fixed the background pattern position & added text size when there is more 10 numbers

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,6 @@ import Dark_Mode from "@/components/Dark_Mode";
 const workSans = Work_Sans({ subsets: ["latin"] });
 
 export default function Home() {
-  // useEffect(() => {
-  //   document.documentElement.classList.add("dark");
-  // }, []);
-
   const [value, setValue] = useState("");
   const [value2, setValue2] = useState("");
   const [result, setResult] = useState(false);
@@ -24,7 +20,7 @@ export default function Home() {
 
       <div
         id="background-pattern"
-        className="absolute w-full h-full duration-200 pointer-events-none -z-10"
+        className="absolute top-0 left-0 w-full h-full duration-200 pointer-events-none -z-10"
       ></div>
       <div className="absolute -translate-x-1/2 rounded-b-full -top-56 left-1/2 bg-dark-high-emphasis w-96 h-60 -z-20 blur-3xl"></div>
 
@@ -38,7 +34,7 @@ export default function Home() {
             value={value.length == 0 ? "0" : value}
             className={`w-full font-light text-right bg-transparent opacity-70 ${
               result ? "text-2xl" : "text-5xl pb-3"
-            }`}
+            } ${value.toString().length > 10 && "!text-3xl"}`}
             style={{ transition: "font-size .3s, padding .3s" }}
           />
           {result && (
@@ -48,7 +44,7 @@ export default function Home() {
               transition={{ duration: 0.5, delay: 0.1 }}
               type="text"
               readOnly={true}
-              value={value2}
+              value={value2.toLocaleString()}
               className={`w-full pb-3 font-light text-right bg-transparent opacity-70 ${
                 value2.toString().length > 10 ? "text-3xl" : "text-5xl"
               }`}
